e5e619f56ecda2468c7b02ac85efed52616165de
radoslawc <radoslawc@gmail.com>
2013-02-13 15:05:41 +0100

    initial commit

---
+++ portal/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'portal.views.home', name='home'),
+    # url(r'^portal/', include('portal.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^statinfo/$', 'accounts.views.stat_info'),                                                                                                                                 
+#(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'myapp/login.html'}),
+
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page' : '/accounts/login'}),
+    (r'^$', 'accounts.views.main')
+
+)
